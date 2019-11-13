f4c458a27741579564473881eddc55755017522f
dimas-flj <dimasflj@gmail.com>
2019-11-13 12:05:08 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/learn/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/learn/cursomc/domain/PagamentoComBoleto.java
@@ -5,13 +5,17 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.learn.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento implements Serializable {
 	private static final long serialVersionUID = -6862697505409867012L;
 	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern="dd/MM/yyyy")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {}
